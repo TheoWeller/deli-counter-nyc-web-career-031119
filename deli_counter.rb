37327d112290a6fcddb2c102fde23f568113d320
Theo Weller <weller.theodor@gmail.com>
2019-02-22 22:34:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,33 @@
-katz_deli = []
-
-def line(katz_deli)
-  if katz_deli.length < 1
+def line(people)
+  if people.length == 0
     puts "The line is currently empty."
   else
-    puts "#{katz_deli}"
+   output = "The line is currently:"
+    i = 0
+   while i < people.length
+    name = people[i]
+    output += " #{i+1}. #{name}"
+       i += 1
+    end
+     puts output
   end
 end
 
-#def take_a_number(katz_deli, name)
-    #  katz_deli.push(name)
-#puts "Welcome, #{name}. You are number #{line_position} in line."
-#end
+def take_a_number(people, name)
+  if people == []
+    puts "Welcome, #{name}. You are number 1 in line."
+     people << name
+  else
+    puts "Welcome, #{name}. You are number #{people.length+1} in line."
+    people << name
+  end
+end
+
+def now_serving(people)
+   if people == []
+     puts "There is nobody waiting to be served!"
+   else
+     puts "Currently serving #{people[0]}."
+     people.shift
+   end
+end
